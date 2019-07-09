d6fee4da3ffa6319bf002d69434cba5a97cb7bed
Derayan Bima Alamsyah <derayan.bima@dana.id>
2019-07-09 14:01:04 +0700

    initial commit

--- lib/model/card_prices.dart
+++ lib/model/card_prices.dart
@@ -1,16 +1,16 @@
 
 class Card_prices {
 
-  String cardmarket_price;
-  String tcgplayer_price;
-  String ebay_price;
-  String amazon_price;
+	String cardmarket_price;
+	String tcgplayer_price;
+	String ebay_price;
+	String amazon_price;
 
-	Card_prices.fromJsonMap(Map<String, dynamic> map): 
-		cardmarket_price = map["cardmarket_price"],
-		tcgplayer_price = map["tcgplayer_price"],
-		ebay_price = map["ebay_price"],
-		amazon_price = map["amazon_price"];
+	Card_prices.fromJsonMap(Map<String, dynamic> map):
+				cardmarket_price = map["cardmarket_price"],
+				tcgplayer_price = map["tcgplayer_price"],
+				ebay_price = map["ebay_price"],
+				amazon_price = map["amazon_price"];
 
 	Map<String, dynamic> toJson() {
 		final Map<String, dynamic> data = new Map<String, dynamic>();
