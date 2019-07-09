f627cadc670c180be02e4fd815ce48d2552bd2c3
Derayan Bima Alamsyah <derayan.bima@dana.id>
2019-07-09 13:59:10 +0700

    initial commit

---
+++ lib/main_page.dart
@@ -0,0 +1,166 @@
+import 'package:flutter/material.dart';
+import 'detail_page.dart';
+import 'model/card.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'model/response.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
+
+void main() => runApp(MyApp());
+
+Future<Response> fetchCard() async{
+  final response = await http.get('https://db.ygoprodeck.com/api/v5/cardinfo.php?num=100');
+  if(response.statusCode == 200){
+    return Response.fromJsonMap(json.decode(response.body));
+  }else{
+    throw Exception('Failed to load data');
+  }
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Final Task Eudeka',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: MyHomePage(title: 'YugiOh!'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: <Widget>[
+          PopupMenuButton<int>(
+            itemBuilder: (context) => [
+              PopupMenuItem<int>(
+                child: Text('Keluar'),
+                value: 1,
+              )
+            ],
+            onSelected: (value){
+              _showDialog();
+            },
+          )
+        ],
+      ),
+      body: _GridView(data: fetchCard(),),
+    );
+  }
+
+  void _showDialog(){
+    showDialog(
+        context: context,
+        builder: (BuildContext context){
+          return AlertDialog(
+            title: Text('Perhatian'),
+            content: Text('Apakah anda yakin ingin keluar?'),
+            actions: <Widget>[
+              FlatButton(
+                  onPressed: (){
+                    exit(0);
+                  },
+                  child: Text('Ya')
+              ),
+              FlatButton(
+                  onPressed: (){
+                    Navigator.pop(context);
+                  },
+                  child: Text('Tidak')
+              )
+            ],
+          );
+        }
+    );
+  }
+}
+
+class _CardItemView extends StatelessWidget{
+
+  final YugiohCard data;
+
+  _CardItemView({Key key, this.data}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        child: InkWell(
+          onTap: (){
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => DetailApp(data: data,)),
+            );
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Expanded(
+                  child: Image.network(data.card_images[0].image_url_small,),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 8),
+                      child: Text(
+                        data.name,
+                        style: TextStyle(fontSize: 16),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        )
+    );
+  }
+}
+
+class _GridView extends StatelessWidget{
+
+  final Future<Response> data;
+  _GridView({Key key, this.data}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Response> (
+      future: data,
+      builder: (context, snapshot){
+        if(!snapshot.hasData){
+          return LinearProgressIndicator();
+        }else if(snapshot.hasError){
+          return Text("${snapshot.error}");
+        }else{
+          return GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            itemBuilder: (context, index){
+              return _CardItemView(data: YugiohCard.fromJsonMap(snapshot.data.data[index]),);
+            },
+            itemCount: snapshot.data.data.length,
+          );
+        }
+      },
+    );
+  }
+}
